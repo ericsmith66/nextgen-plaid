2248a4a542707b2a4c82026f2123cd88a23da30d
ericsmith66 <ericsmith66@me.com>
2025-12-13 15:54:43 -0600

    Fix navigation visibility on mission control: show all links for owner users by adding owner? helper method

--- app/components/navigation_component.rb
+++ app/components/navigation_component.rb
@@ -9,6 +9,12 @@ class NavigationComponent < ViewComponent::Base
     @current_user&.admin?
   end
 
+  def owner?
+    return false unless @current_user
+    owner_email = ENV["OWNER_EMAIL"].presence || "ericsmith66@me.com"
+    @current_user.email == owner_email
+  end
+
   def authenticated?
     @current_user.present?
   end
