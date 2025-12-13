e75b6e3cce2628fcf128f91e121d94022554f7a2
ericsmith66 <ericsmith66@me.com>
2025-12-13 13:59:41 -0600

    Fix mission control error: pass sync_logs to MissionControlComponent

--- app/components/mission_control_component.rb
+++ app/components/mission_control_component.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class MissionControlComponent < ViewComponent::Base
-  def initialize(plaid_items:, transactions:, recurring_transactions:, accounts:, holdings:)
+  def initialize(plaid_items:, transactions:, recurring_transactions:, accounts:, holdings:, sync_logs:)
     @plaid_items = plaid_items
     @transactions = transactions
     @recurring_transactions = recurring_transactions
     @accounts = accounts
     @holdings = holdings
+    @sync_logs = sync_logs
   end
 end
